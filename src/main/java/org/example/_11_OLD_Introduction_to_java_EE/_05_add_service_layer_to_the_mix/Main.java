7e2671a34b047f5e4b37fd9df3aef35aefc1d448
Alex <alex@stl-horizon.com>
2024-10-31 16:19:14 +0300

    Exploring Java EE Basics - Add Service Layer to the mix

---
+++ src/main/java/org/example/_11_OLD_Introduction_to_java_EE/_05_add_service_layer_to_the_mix/Main.java
@@ -0,0 +1,6 @@
+package org.example._11_OLD_Introduction_to_java_EE._05_add_service_layer_to_the_mix;
+/*
+ * Exploring Java EE Basics - Adding a Service Layer to the mix
+ */
+public class Main {
+}
