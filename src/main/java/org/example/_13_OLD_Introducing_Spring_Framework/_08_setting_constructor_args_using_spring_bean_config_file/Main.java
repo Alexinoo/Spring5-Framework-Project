e37fc5e735db0259d2ab0a3bb0b14e679edc2944
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 16:01:51 +0300

    Setting Constructor Arguments using the Spring bean config file

---
+++ src/main/java/org/example/_13_OLD_Introducing_Spring_Framework/_08_setting_constructor_args_using_spring_bean_config_file/Main.java
@@ -0,0 +1,6 @@
+package org.example._13_OLD_Introducing_Spring_Framework._08_setting_constructor_args_using_spring_bean_config_file;
+/*
+ * Setting Constructor Arguments using the Spring bean config file
+ */
+public class Main {
+}
