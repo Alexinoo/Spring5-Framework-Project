606535b2d3b527fa85300a938fe21279f6aaaf81
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 16:39:00 +0300

    Spring Bean instantiation using Static Factory and Instance Factory methods

---
+++ src/main/java/org/example/_15_OLD_Bean_Configuration/_06_spring_bean_instantiation_using_static_factory_and_instance_factory_methods/Main.java
@@ -0,0 +1,6 @@
+package org.example._15_OLD_Bean_Configuration._06_spring_bean_instantiation_using_static_factory_and_instance_factory_methods;
+/*
+ * Spring Bean instantiation using Static Factory and Instance Factory methods
+ */
+public class Main {
+}
