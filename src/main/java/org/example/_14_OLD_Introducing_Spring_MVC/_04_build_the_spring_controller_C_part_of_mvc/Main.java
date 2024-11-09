6d56ac3f75551f1b381bd5eb0cb7a8414cf07e4a
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 16:18:41 +0300

    Building the Spring Controller - "C" part of MVC

---
+++ src/main/java/org/example/_14_OLD_Introducing_Spring_MVC/_04_build_the_spring_controller_C_part_of_mvc/Main.java
@@ -0,0 +1,6 @@
+package org.example._14_OLD_Introducing_Spring_MVC._04_build_the_spring_controller_C_part_of_mvc;
+/*
+ * Building the Spring Controller - "C" part of MVC
+ */
+public class Main {
+}
