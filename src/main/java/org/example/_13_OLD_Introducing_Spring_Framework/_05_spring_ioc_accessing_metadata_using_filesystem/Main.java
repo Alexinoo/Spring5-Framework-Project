75de1103da03c96ac81c9b8f00537ab0f2d96b81
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 15:55:57 +0300

    Spring IoC container : Accessing metadata from the File system

---
+++ src/main/java/org/example/_13_OLD_Introducing_Spring_Framework/_05_spring_ioc_accessing_metadata_using_filesystem/Main.java
@@ -0,0 +1,7 @@
+package org.example._13_OLD_Introducing_Spring_Framework._05_spring_ioc_accessing_metadata_using_filesystem;
+
+/*
+ * Spring IoC container : Accessing metadata from the File system
+ */
+public class Main {
+}
