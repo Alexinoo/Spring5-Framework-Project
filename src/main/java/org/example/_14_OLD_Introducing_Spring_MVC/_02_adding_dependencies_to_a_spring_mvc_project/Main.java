8dd2559957f76230a37acccacf745cfc6f14f09b
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 16:12:39 +0300

    Adding Dependencies in an alternative way in a Spring MVC Project

---
+++ src/main/java/org/example/_14_OLD_Introducing_Spring_MVC/_02_adding_dependencies_to_a_spring_mvc_project/Main.java
@@ -0,0 +1,6 @@
+package org.example._14_OLD_Introducing_Spring_MVC._02_adding_dependencies_to_a_spring_mvc_project;
+/*
+ * Adding Dependencies in an alternative way in a Spring MVC Project
+ */
+public class Main {
+}
