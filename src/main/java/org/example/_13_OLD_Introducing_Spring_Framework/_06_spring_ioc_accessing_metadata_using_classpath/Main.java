c091cd7978b1c61555f33f1e6311d09cd91dc281
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 15:57:13 +0300

    Spring IoC container : Accessing metadata from the Class Path

---
+++ src/main/java/org/example/_13_OLD_Introducing_Spring_Framework/_06_spring_ioc_accessing_metadata_using_classpath/Main.java
@@ -0,0 +1,6 @@
+package org.example._13_OLD_Introducing_Spring_Framework._06_spring_ioc_accessing_metadata_using_classpath;
+/*
+ * Spring IoC container : Accessing metadata from the Class Path
+ */
+public class Main {
+}
