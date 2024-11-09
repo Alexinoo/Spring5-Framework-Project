dc88f6fe7f5dccf3b25274ad3ac6cd993533be6b
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 16:49:43 +0300

    Pros and Cons of Spring Configuration : Annotations vs XML

---
+++ src/main/java/org/example/_16_Configuring_Spring_with_Annotations/_01_annotations_vs_xml_configurations/Main.java
@@ -0,0 +1,7 @@
+package org.example._16_Configuring_Spring_with_Annotations._01_annotations_vs_xml_configurations;
+
+/*
+ * Pros and Cons of Spring Configuration : Annotations vs XML
+ */
+public class Main {
+}
