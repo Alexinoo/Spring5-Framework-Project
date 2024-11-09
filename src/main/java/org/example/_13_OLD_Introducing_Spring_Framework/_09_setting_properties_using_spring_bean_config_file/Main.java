deeae487fc612775c31f859e4e8a4396f1ef0a16
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 16:03:31 +0300

    Setting Properties using the Spring bean config file

---
+++ src/main/java/org/example/_13_OLD_Introducing_Spring_Framework/_09_setting_properties_using_spring_bean_config_file/Main.java
@@ -0,0 +1,6 @@
+package org.example._13_OLD_Introducing_Spring_Framework._09_setting_properties_using_spring_bean_config_file;
+/*
+ * Setting Properties Using the Spring bean config file
+ */
+public class Main {
+}
