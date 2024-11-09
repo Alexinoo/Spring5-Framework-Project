8fbe481e198f43dae7aa7402b62bf4d799154eb5
Alex Mwangi <alex@stl-horizon.com>
2024-11-09 17:01:05 +0300

    Autowiring Spring Beans - Creating Project Infrastructure

---
+++ src/main/java/org/example/_16_Configuring_Spring_with_Annotations/_02_autowire_spring_beans_using_annotations/Main.java
@@ -0,0 +1,6 @@
+package org.example._16_Configuring_Spring_with_Annotations._02_autowire_spring_beans_using_annotations;
+/*
+ * Autowiring Spring Beans - Creating Project Infrastructure
+ */
+public class Main {
+}
